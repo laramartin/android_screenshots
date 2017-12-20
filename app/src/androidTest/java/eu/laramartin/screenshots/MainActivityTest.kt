20ec6eec521a8ae90e05e6ba774d76c372332ddd
Lara Martin <lara.martin.carretero@gmail.com>
2017-12-20 14:13:24 +0100

    updated Espresso test to take screenshots

--- app/src/androidTest/java/eu/laramartin/screenshots/MainActivityTest.kt
+++ app/src/androidTest/java/eu/laramartin/screenshots/MainActivityTest.kt
@@ -1,5 +1,6 @@
 package eu.laramartin.screenshots
 
+import android.os.SystemClock
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.assertion.ViewAssertions.matches
@@ -7,20 +8,41 @@ import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import org.junit.BeforeClass
+import org.junit.ClassRule
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import tools.fastlane.screengrab.Screengrab
+import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy
+import tools.fastlane.screengrab.locale.LocaleTestRule
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
-    @Rule
-    @JvmField
-    val mActivityRule = ActivityTestRule(MainActivity::class.java)
+    companion object {
+
+        @get:ClassRule
+        @JvmStatic
+        val localeTestRule = LocaleTestRule()
+
+        @BeforeClass
+        @JvmStatic
+        fun beforeAll() {
+            Screengrab.setDefaultScreenshotStrategy(UiAutomatorScreenshotStrategy())
+        }
+    }
+
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
 
     @Test
     fun displayHello() {
+        SystemClock.sleep(1000)
+        Screengrab.screenshot("01_initial_screen")
         onView(withId(R.id.button)).perform(click())
+        SystemClock.sleep(1000)
+        Screengrab.screenshot("02_displayed_text_screen")
         onView(withId(R.id.container)).check(matches(isDisplayed()))
     }
 }
